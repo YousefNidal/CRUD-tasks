aa109526716c3d94174405a3a9566943057aa0dc
YousefNidal <ameryousef2007@gmail.com>
2024-07-06 12:23:43 +0300

    deletion of guard

--- projects/admin/src/app/dashboard/dashboard-routing.module.ts
+++ projects/admin/src/app/dashboard/dashboard-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 import { AdminGuard } from '../core/guards/admin.guard';
-import { AdminLoginRejectGuard } from '../core/guards/admin-login-reject.guard';
+
 
 
 
 const routes: Routes = [
   {path:'', component:LayoutComponent,
     
-    canActivateChild:[AdminGuard, AdminLoginRejectGuard]
+    canActivateChild:[AdminGuard]
     ,children:[
     {path:'tasks', 
       loadChildren: () => import(`./tasks-admin/tasks-admin.module`).then(m => m.TasksAdminModule)
